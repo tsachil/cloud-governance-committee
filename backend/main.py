1edf0dabd5814cf425ddb2e2e4ce7fa36c4826a9
tsachi <tsachil@gmail.com>
2026-01-20 21:43:47 +0200

    security: upgrade python to 3.11, add non-root user, restrict CORS

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Cloud Governance Committee API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "http://localhost:8080", "http://127.0.0.1:5173", "http://127.0.0.1:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
